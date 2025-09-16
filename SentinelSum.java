981473bb7f3df0ea2af406d29bbaf1d59707e77b
anabdalla1 <anabdalla1@my.waketech.edu>
2025-09-16 10:18:41 -0400

    boonbeam ice cream

---
+++ SentinelSum.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class SentinelSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // get user input
+        String userIceCream = "Enter a flavor, any flavor";
+    }
+}
